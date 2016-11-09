03a42c0fd3031afd1583e3d0dd54c21d55842037
rolandcase <rcs25@students.uwf.edu>
2016-11-09 14:04:30 -0600

    Add files via upload

---
+++ Story.java
@@ -0,0 +1,16 @@
+/*
+Story Methods
+*/
+   import java.util.Random;
+   
+   public class Story
+      {
+       //
+       //
+      public void story1()
+         {
+            System.out.println("You've just spoke with a client and you need to report back with Quartz \n");
+            System.out.println("Head to the forest for snacks! ");
+         }
+         
+      }
