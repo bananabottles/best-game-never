fd16d3dd2a08cf8f471e2709c6d524cda1fb6935
Daniel <dpp9@students.uwf.edu>
2016-11-21 11:09:59 -0600

    Add files via upload

--- Story.java
+++ Story.java
@@ -1,16 +1,20 @@
-/*
-Story Methods
-*/
-   import java.util.Random;
+
+
+public class Story
+{
    
-   public class Story
-      {
-       //
-       //
-      public void story1()
-         {
-            System.out.println("You've just spoke with a client and you need to report back with Quartz \n");
-            System.out.println("Head to the forest for snacks! ");
-         }
-         
-      }
+   
+   
+   public Story
+   {
+   
+   }
+   
+   public loadStory(Area a, Player p)
+   {
+   
+   
+   }
+   
+   
+}
